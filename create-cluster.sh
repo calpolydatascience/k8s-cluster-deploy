592131aab8b6a163c38c0f5543a9406402f28eb3
Dwight Townsend <townsenddw@gmail.com>
2018-03-08 16:47:07 -0800

    working efs mounting per user

--- create-cluster.sh
+++ create-cluster.sh
@@ -7,18 +7,10 @@ export KOPS_STATE_STORE=s3://clusters.k8s.thedwightway.com
 kops create cluster \
   --zones us-west-2a \
   --node-count=2 \
-  --node-size=t2.micro \
+  --node-size=t2.medium \
   --master-size=t2.micro \
   --ssh-public-key ./keys/kops_rsa.pub \
   --topology private \
   --networking weave \
   ${NAME} \
   --yes 
-
-# Create tiller service account, cluster-side package manager
-kubectl --namespace kube-system create serviceaccount tiller
-helm init --service-account tiller
-
-# Create Services
-## Kubernetes Dashboard
-kubectl create -f https://raw.githubusercontent.com/kubernetes/dashboard/master/src/deploy/recommended/kubernetes-dashboard.yaml
