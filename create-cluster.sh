1762b7bacb2b9363ccea4947511604803a54326d
Dwight Townsend <townsenddw@gmail.com>
2018-03-26 16:23:44 -0700

    cleaning up... getting ready for first iteration run

--- create-cluster.sh
+++ create-cluster.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-export NAME=beta.k8s.local
-export KOPS_STATE_STORE=s3://clusters.k8s.thedwightway.com
+export NAME=calpolydatascience.k8s.local
+export KOPS_STATE_STORE=s3://clusters.k8s.calpolydatascience.org
 
 # Create Cluster
 kops create cluster \
   --zones us-west-2a \
-  --node-count=2 \
-  --node-size=t2.medium \
+  --node-count=1 \
+  --node-size=r4.large \
   --master-size=t2.micro \
   --ssh-public-key ./keys/kops_rsa.pub \
   --topology private \
